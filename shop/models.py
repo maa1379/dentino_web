b62df0bb1f74bedf2baaab91129351bfb5d88335
elyas <elyashedayat1380@gmail.com>
2022-01-04 00:39:25 -0800

    shop

--- shop/models.py
+++ shop/models.py
@@ -13,7 +13,8 @@ class Category(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(decimal_places=2, max_digits=10)
-    sell = models.DecimalField(decimal_places=2, max_digits=10, null=True, blank=True)
+    sell = models.DecimalField(decimal_places=2, max_digits=10, blank=True, null=True)
+    discount_percent = models.IntegerField()
     image = models.ImageField(upload_to="images/produce/")
     image2 = models.ImageField(upload_to="images/produce/", blank=True)
     image3 = models.ImageField(upload_to="images/produce/", blank=True)
@@ -25,7 +26,11 @@ class Product(models.Model):
     category = models.ForeignKey(
         Category, on_delete=models.CASCADE, related_name="produce"
     )
-    is_send=models.BooleanField(default=False)
+
+    def save(self, *args, **kwargs):
+        count = (self.price * self.discount_percent) / 100
+        self.sell = self.price - count
+        return super(Product, self).save()
 
     def __str__(self):
         return self.name
